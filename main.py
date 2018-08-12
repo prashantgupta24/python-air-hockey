836db5be70038e01d39e06099259aeae935e4613
Prashant Gupta <prashantgupta@us.ibm.com>
2018-08-12 00:39:27 -0700

    ball bouncing

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class App:
         pyxel.init(256, 256)
         self.border = 15
 
+        self.isGameActive = True
+
         #paddle dimensions
         self.pLength = 20
         self.pMovingDistance = 5 #pixels to move paddle on key press
@@ -24,93 +26,86 @@ class App:
         self.cx = pyxel.width / 2
         self.cy = pyxel.height / 2
         self.cr = 5
-        # self.cm = 1
-        # self.cc = 50
-        # self.cxMovingLength = randint(-10,10)
-        # self.cyMovingLength = randint(-10,10)
-        self.vx = 1
-        self.vy = 2
+
+        #ball moving parameters
+        self.vx = -1
+        self.vy = -2
 
         pyxel.run(self.update, self.draw)
 
     #def distanceOfBallFromPoint()
-
+    
     def checkBallCollision(self):
 
         hasBallCollided = False
 
         topPointY = self.cy - self.cr
-
         bottomPointY = self.cy + self.cr
 
         leftPointX = self.cx - self.cr
-
         rightPointX = self.cx + self.cr
 
-        #print(topPointY)
         #top or down
-        if topPointY <= self.border or bottomPointY >= pyxel.height - self.border:
-        #if topPointY <= self.border:
+        if topPointY < self.border or bottomPointY > pyxel.height - self.border:
             hasBallCollided = True
             self.vy = self.vy * -1
-            print("collided!")
+            print("collided top/bottom!")
 
-        elif leftPointX <= self.p1x or rightPointX >= pyxel.width - self.border:
-            hasBallCollided = True
-            self.vx = self.vx * -1
-            print("collided!")
+        #hitting either paddles
+        # if self.p1x >= leftPointX and self.p1x < self.cx:
+        #     if self.p1y > bottomPointY or
+        #
+        # if self.cx < self.p1x or rightPointX > self.p2x:
+        #     hasBallCollided = True
+        #     self.vx = self.vx * -1
+        #     print("collided left/right!")
 
-        #return hasBallCollided
-        #paddle
-        #if distanceOfBallFromPoint()
+        #entering the realm of p1
+        if leftPointX <= self.p1x < self.cx:
+            if bottomPointY < self.p1y or topPointY > self.p1y + self.pLength:
+                print("missed!")
+                self.isGameActive = False
+            else:
+                print("collided with left paddle!")
+                self.vx = self.vx * -1
 
     def update(self):
-        #print("update")
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
-        if pyxel.btn(pyxel.KEY_W):
-            if self.p1y > self.border:
-                self.p1y = (self.p1y - self.pMovingDistance)
+        if self.isGameActive:
+            if pyxel.btn(pyxel.KEY_W):
+                if self.p1y > self.border:
+                    self.p1y = (self.p1y - self.pMovingDistance)
 
-        if pyxel.btn(pyxel.KEY_S):
-            if self.p1y + self.pLength + self.pMovingDistance < pyxel.height-self.border:
-                self.p1y = (self.p1y + self.pMovingDistance)
+            if pyxel.btn(pyxel.KEY_S):
+                if self.p1y + self.pLength + self.pMovingDistance < pyxel.height-self.border:
+                    self.p1y = (self.p1y + self.pMovingDistance)
 
-        if pyxel.btn(pyxel.KEY_UP):
-            if self.p2y > self.border:
-                self.p2y = (self.p2y - self.pMovingDistance)
+            if pyxel.btn(pyxel.KEY_UP):
+                if self.p2y > self.border:
+                    self.p2y = (self.p2y - self.pMovingDistance)
 
-        if pyxel.btn(pyxel.KEY_DOWN):
-            if self.p2y + + self.pLength + self.pMovingDistance < pyxel.height-self.border:
-                self.p2y = (self.p2y + self.pMovingDistance)
+            if pyxel.btn(pyxel.KEY_DOWN):
+                if self.p2y + + self.pLength + self.pMovingDistance < pyxel.height-self.border:
+                    self.p2y = (self.p2y + self.pMovingDistance)
 
-        self.checkBallCollision()
-        # if self.checkBallCollision():
-        #     self.cMovingLength = self.cMovingLength * -1
-            # self.cc = self.cy - self.cm * self.cx
+            self.checkBallCollision()
 
-        # if not self.checkBallCollision():
-        #     self.cx = self.cx + self.cMovingLength
-        #     self.cy = self.cm * self.cx + self.cc
-        #     print(self.cMovingLength)
-        # else:
-        #     self.cMovingLength = self.cMovingLength * -1
-        #     self.cx = self.cx + self.cMovingLength
-        #     self.cy = self.cm * self.cx + self.cc
-        #     print(self.cMovingLength)
-        self.cx = self.cx + self.vx
-        self.cy = self.cy + self.vy
+            self.cx = self.cx + self.vx
+            self.cy = self.cy + self.vy
 
-        print(f'x is {self.cx} and y is {self.cy}')
+            print(f'x is {self.cx} and y is {self.cy}')
 
     def draw(self):
-        #print("draw")
-        pyxel.cls(0)
 
-        pyxel.line(self.p1x, self.p1y, self.p1x, self.p1y + self.pLength, 7)
-        pyxel.line(self.p2x, self.p2y, self.p2x, self.p2y + self.pLength, 7)
+        if self.isGameActive:
 
-        pyxel.circ(self.cx, self.cy, self.cr, 7)
+            pyxel.cls(0)
+
+            pyxel.line(self.p1x, self.p1y, self.p1x, self.p1y + self.pLength, 7)
+            pyxel.line(self.p2x, self.p2y, self.p2x, self.p2y + self.pLength, 7)
+
+            pyxel.circ(self.cx, self.cy, self.cr, 7)
 
 App()
