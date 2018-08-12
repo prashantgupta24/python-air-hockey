bf3eb18dd6ac5c436b87509222fc2fb00c7b0139
Prashant Gupta <prashantgupta@us.ibm.com>
2018-08-12 13:07:06 -0700

    'edge' cases

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 from random import randint
 
 import pyxel
@@ -36,8 +35,14 @@ class App:
         #ball moving parameters
         # self.vx = randint(-2,-1)
         # self.vy = randint(-2,-1)
-        self.vx = 1
-        self.vy = 2
+        self.vx = -2.5
+        self.vy = -3
+
+    def properHit(self, paddleY):
+        if paddleY <= self.cy <= paddleY + self.pLength :
+            return True
+
+        return False
 
     def checkBallCollision(self):
 
@@ -53,28 +58,59 @@ class App:
         if topPointY < self.border or bottomPointY > pyxel.height - self.border:
             hasBallCollided = True
             self.vy = self.vy * -1
-            print("collided top/bottom!")
+            #print("collided top/bottom!")
 
         #entering the realm of p1
         if leftPointX <= self.p1x and self.vx < 0:
             if bottomPointY < self.p1y or topPointY > self.p1y + self.pLength:
                 pass
             else:
-                #determine angle of hit
                 print("collided with left paddle!")
-                self.vx = self.vx * -1
+                print(f'x is {self.cx} and y is {self.cy}')
+                #determine angle of hit
+                if not self.properHit(self.p1y):
+                    if self.vy > 0:
+                        print("ball coming from top!")
+                        # hitting top edge
+                        if self.cy < self.p1y:
+                            print("hit top edge of paddle 1!")
+                            #print(f'vs is {self.vx} and vy is {self.vy}')
+                            #self.vx, self.vy = self.vy, self.vx
+                            self.vx = self.vx * -1
+                            self.vy = self.vy * -1
+                            self.vx = self.vx - 0.1
+                        else:
+                            print("hit bottom edge of paddle 1!")
+                            self.vx = self.vx * -1
+                    else:
+                        print("ball coming from bottom")
+                        # hitting top edge
+                        if self.cy < self.p1y:
+                            print("hit top edge of paddle 1!")
+                            self.vx = self.vx * -1
+                            self.vx = self.vx - 0.1
+                        else:
+                            print("hit bottom edge of paddle 1!")
+                            self.vx = self.vx * -1
+                            self.vy = self.vy * -1
+                            self.vx = self.vx - 0.1
+                else:
+                    print("proper hit!")
+                    self.vx = self.vx * -1
 
         #entering the realm of p2
         if rightPointX >= self.p2x and self.vx > 0:
-            if bottomPointY < self.p2y or topPointY > self.p2y + self.pLength:
-                pass
-            else:
-                #determine angle of hit
-                print("collided with right paddle!")
-                self.vx = self.vx * -1
+            # if bottomPointY < self.p2y or topPointY > self.p2y + self.pLength:
+            #     pass
+            # else:
+            #     #determine angle of hit
+            #     self.vx = randint(1,3)
+            #     self.vy = randint(1,3)
+            print("collided with right paddle!")
+            self.vx = self.vx * -1
 
         #missed both
-        if self.p2x < self.cx < self.p1x:
+        if self.cx > self.p2x or self.cx < self.p1x:
             print("missed!")
             self.isGameActive = False
 
@@ -108,7 +144,7 @@ class App:
             self.cx = self.cx + self.vx
             self.cy = self.cy + self.vy
 
-            print(f'x is {self.cx} and y is {self.cy}')
+            #print(f'x is {self.cx} and y is {self.cy}')
 
     def draw(self):
 
@@ -121,4 +157,9 @@ class App:
 
             pyxel.circ(self.cx, self.cy, self.cr, 7)
 
+        else:
+            pyxel.cls(0)
+
+            pyxel.line(self.p1x, self.p1y, self.p1x, self.p1y + self.pLength, 7)
+            pyxel.line(self.p2x, self.p2y, self.p2x, self.p2y + self.pLength, 7)
 App()
